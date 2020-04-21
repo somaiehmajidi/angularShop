f23d84fdf91922151aa7dac93cc49fdc728abc73
somaiehmajidi <somaieh.majidi@gmail.com>
2020-04-21 17:05:25 +0430

    created 100 fake entries using faker methods

---
+++ server/generate.js
@@ -0,0 +1,19 @@
+var faker = require('faker');
+
+var database = { products: []};
+
+for (var i = 1; i<= 100; i++) {
+  database.products.push({
+    id: i,
+    name: faker.commerce.productName(),
+    description: faker.lorem.sentences(),
+    price: faker.commerce.price(),
+    imageUrl: "https://source.unsplash.com/1600x900/?product",
+    quantity: faker.random.number(),
+    date: faker.date.recent(),
+    producer: faker.company.companyName()
+  });
+}
+
+console.log(JSON.stringify(database));
+
