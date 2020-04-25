1b5b84cd32338fabf7e787fd62c361524128915f
sam <sam@somaiehs-MacBook-Pro.local>
2020-04-25 21:08:12 +0430

    added single product page

---
+++ src/app/product/pro/pro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pro',
+  templateUrl: './pro.component.html',
+  styleUrls: ['./pro.component.css']
+})
+export class ProComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
